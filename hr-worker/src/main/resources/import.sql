4df83463835c6d9ed9d6c498b399c086447040bc
monthalcantara <montival_junior@yahoo.com.br>
2020-11-12 19:01:40 -0300

    Adicionada arquivo de preenchimento de banco para testes

---
+++ hr-worker/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO worker (name, daily_income) VALUES ('Bob', 200.00);
+INSERT INTO worker (name, daily_income) VALUES ('Maria', 300.00);
+INSERT INTO worker (name, daily_income) VALUES ('Alex', 250.00);
