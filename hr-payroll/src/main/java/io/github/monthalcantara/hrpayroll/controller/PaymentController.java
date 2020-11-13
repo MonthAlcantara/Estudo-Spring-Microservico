f5631e0806481001be47c333e1037baff5676509
monthalcantara <montival_junior@yahoo.com.br>
2020-11-13 10:53:50 -0300

    implementado servie hr-payroll

---
+++ hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package io.github.monthalcantara.hrpayroll.controller;
+
+import io.github.monthalcantara.hrpayroll.model.Payment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+@Validated
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @GetMapping("/{id}/days/{days}")
+    public ResponseEntity findPayment(@PathVariable("id") Long id, @PathVariable("days") @PositiveOrZero @Max(31) Integer days) {
+        System.out.println(id + days);
+        return ResponseEntity.ok(new Payment("Bob", BigDecimal.valueOf(300.00), days));
+    }
+
+}
---
+++ hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/model/Payment.java
@@ -0,0 +1,52 @@
+package io.github.monthalcantara.hrpayroll.model;
+
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Payment implements Serializable {
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private BigDecimal dailyIncome;
+
+    @PositiveOrZero
+    @Max(31)
+    private Integer days;
+
+    @Deprecated
+    public Payment() {
+    }
+
+    public Payment(@NotBlank String name, @NotNull BigDecimal dailyIncome, @PositiveOrZero @Max(31) Integer days) {
+        Assert.isTrue(days > 0, "The number of days cannot be negative");
+        Assert.isTrue(days < 31, "The number of days cannot be greater than 31");
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+        this.days = days;
+    }
+
+    public BigDecimal getTotal() {
+        return this.dailyIncome.multiply(BigDecimal.valueOf(this.days));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+}
