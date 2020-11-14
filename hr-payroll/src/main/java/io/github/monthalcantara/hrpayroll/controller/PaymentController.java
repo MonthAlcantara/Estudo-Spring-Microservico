d2a66902835d3c273e96612d069210e98e57cf38
monthalcantara <montival_junior@yahoo.com.br>
2020-11-14 14:05:40 -0300

    testando merges

--- hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/controller/PaymentController.java
@@ -37,5 +37,6 @@ public class PaymentController {
         //1
         WorkerResponse response = restTemplate.getForObject(workerHost + "/workers/{id}", WorkerResponse.class, uriVariables);
         return ResponseEntity.ok(new Payment(response.getName(), response.getDailyIncome(), days));
+        //Gerando teste
     }
 }
