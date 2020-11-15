32008d28ec3b711f8c28979f681b46de39cfa860
monthalcantara <montival_junior@yahoo.com.br>
2020-11-14 23:29:54 -0300

    configurada dependencia eureka client

--- hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/io/github/monthalcantara/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package io.github.monthalcantara.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="hr-payroll")
+// @RibbonClient(name="hr-payroll") Como o Eureka ja possui balanceamento de Carga, não preciso mais utilizar o Ribbon
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
